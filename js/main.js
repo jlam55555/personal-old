752f15f4630e62a03205c3f1ee05ab9bf54843aa
Jonathan Lam <jlam55555@gmail.com>
2017-12-03 23:46:33 -0500

    updated styles

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 $(() => {
 
+  // elements
+  var $window = $(window);
+  var $document = $(document);
+  var $body = $("body");
+  var $html = $("html");
+  var $title = $("#title");
+  var $dynamicTerminal = $("#dynamicTerminal");
+  var $cursor = $("#cursor");
+
   // convenience functions
   var random = n => Math.floor(Math.random() * (typeof n === "object" ? n.length : n));
 
+  // onload animation
+  $title.css({margin: ($window.height() - $title.innerHeight())/2 + "px 0"});
+  setTimeout(() => $title.addClass("normalMargins"), 1000);
+
   // dynamic terminal
   var actions = ["brother", "math", "run", "blog", "code", "build", "cube", "bowl", "teach", "learn", "hack"];
-  var $dynamicTerminal = $("#dynamicTerminal");
-  var $cursor = $("#cursor");
   var actionsIndex = random(actions);
   var wordIndex = 0;
   var increasing = true;
